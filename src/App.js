5321a377f01e1e03cb037b4a6e779c8003cb108e
chinh <chinh3571113@gmail.com>
2024-05-10 22:16:57 +0700

    First commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const player = {
+    id: 2, 
+    name: "Test",
+    
+  };
+
+  return (
+    <>
+      <div className='div' >Hello</div>
+      
+    </>
+  );
+}
+
+export default App;
